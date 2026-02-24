9890d8b91c3d443df5da45db3cae95967f54b2b9
josegerm <josegerm@andrew.cmu.edu>
2026-02-24 15:22:14 +0000

    added product

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -58,6 +58,16 @@ export default function QueryProcessor(query: string): string {
     return "Please give numbers.";
   }
 
+  if (query.toLowerCase().includes("multiplied")) {
+    const matches = query.match(/\d+/g);
+    if (matches) {
+      const numbers = matches.map(Number);
+      const product = numbers.reduce((acc, curr) => acc + curr, 1);
+
+      return `${product}`;
+    }
+    return "Please give numbers.";
+  }
 
   return "";
 }
